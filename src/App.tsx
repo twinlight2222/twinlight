c697e844ce8b4fac3c60a95a603ae78b54d618dc
twinlight <info@l-margo.com>
2025-06-12 00:43:34 +0900

    importエラー回避コメントアウト対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import {Routes, Route } from "react-router-dom";
 import TopPage from './TopPage';
-import Diagnosis from "./pages/Diagnosis";
-import Result from "./pages/Result";
-import Chat24h from "./pages/24h";
+// import Diagnosis from "./pages/Diagnosis"; //テスト対応
+// import Result from "./pages/Result";//テスト対応
+// import Chat24h from "./pages/24h";//テスト対応
 import EstelleChat from "./pages/EstelleChat";
 import'./App.css';
 
@@ -12,9 +12,9 @@ function App() {
     return (
         <Routes>
           <Route path="/" element={<TopPage />} />
-          <Route path="/diagnosis" element={<Diagnosis /> } />
-          <Route path="/result"element={<Result />} />
-          <Route path="/24h" element={<Chat24h />} />
+          {/* <Route path="/diagnosis" element={<Diagnosis /> } /> */}
+          {/* <Route path="/result"element={<Result />} /> */}
+          {/* <Route path="/24h" element={<Chat24h />} /> */}
           <Route path="/estelle" element={<EstelleChat />} />
         </Routes>
   )
