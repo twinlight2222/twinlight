11f01a371668ddc400cb30ca83fccccb9eb262f2
twinlight2222 <ask_dc@outlook.com>
2025-07-13 02:11:59 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
+  HEAD
   root: './',
-=======
->>>>>>> master
+
+  master
   plugins: [react()],
 })
